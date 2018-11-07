40353a234043c3429472cc506150aeaefa669455
bamboo <bamboo@milieuinfo.be>
2018-11-07 09:32:03 +0000

    GVR-39 iron-flex-layout import verwijderd

--- vo-template.js
+++ vo-template.js
@@ -1,5 +1,4 @@
 import { LitElement, html } from "./assets/@polymer/lit-element/lit-element.js";
-import './assets/@polymer/iron-flex-layout/iron-flex-layout.js';
 import "./assets/vo-header/dist/vo-header.js";
 import "./assets/vo-footer/dist/vo-footer.js";
 import "./assets/vo-font/dist/vo-font.js";
